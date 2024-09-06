ee61f232d505cf052ec0f1b6496f4415bd9d57c7
Janaína Cazuza <janainamartinscazuza@gmail.com>
2024-09-06 05:16:08 -0300

    Atualizando o final da aula 4

--- scripts/utils/visualization.py
+++ scripts/utils/visualization.py
@@ -9,14 +9,24 @@ import numpy as np
 
 # Função para plotar gráfico de gastos por estado
 def plot_state_spending(data):
-    
+
     # Criando o gráfico
     axis = data.T.plot(figsize=(12,6))
 
-    # Estabelecendo os limites do eixo Y
-    plt.ylim(0, max(data.max()) + 1.1 * max(data.max()))
+    # Definindo uma paleta de cores personalizada
+    colorsofme = ['#F90333', '#50AAA6', '#1155EA', '#EA800A', '#83F921', '#8D9687', '#5E0375']
 
-    # Estabelecendo o eixp x para ticks de 12 meses
+    # Definindo as cores das linhas    
+    for i, line in enumerate(axis.get_lines()):
+        line.set_color(colorsofme[i])
+
+    #Colocando grid no gráfico e cofigurando o grid
+    plt.grid(True, which='major', linestyle='dotted', linewidth='0.5', color='gray')
+    
+    # Estabelecendo os limites do eixo Y
+    plt.ylim(0, max(data.max()) * 1.1)
+
+    # Estabelecendo o eixo x para ticks de 12 meses
     plt.xticks(np.arange(0, len(data.columns), 12), data.columns[::12], rotation=45)
 
     # Configurando o gráfico
